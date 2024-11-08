a076a2485112351a19a02afd2f7616605f276224
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-11-09 00:54:22 +0100

    Minor Update

--- get_data.py
+++ get_data.py
@@ -26,7 +26,6 @@ def get_data():
     url = f'https://github.com/f1db/f1db/releases/download/{last_version}/f1db-csv.zip'
 
     try:
-        download(url, last_version) # always download on server
         with open(last_version_file, 'r') as file:
             content = file.read().strip()
         
